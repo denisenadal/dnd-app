37dffee442d288fa87f2229acfe3d379319b80ba
Denise Nadal <denisenadal@outlook.com>
2020-04-23 19:57:27 -0400

    starting restructure of index page

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,161 +1,148 @@
 <template>
-    <div id="app">
-        <header class="navbar is-primary">
-            <div class="container">
-                <div class="navbar-brand ">
-                    <h1 class="navbar-item title is-4">DND Initiative Tracker
-                    </h1>
-                </div>
-            </div>
-        </header>
-        <main class="section">
-            <div class="container">
-                <div class="columns is-tablet">
-                    <section class="column is-half-tablet is-one-third-desktop">
-                        <div class="card">
-                            <div class="card-content">
-                                <h2 class="title">Add Character</h2>
-                                <div v-if="errors.length"
-                                     class="message is-danger">
-                                    <p class="message-body content">
-                                        <strong>Please correct the following
-                                            error(s):</strong>
-                                        <ul>
-                                            <li v-for="error in errors">
-                                                {{ error }}</li>
-                                        </ul>
-                                    </p>
-                                </div>
-                                <form action=""
-                                      @submit="addChar">
-                                    <div class="field">
-                                        <label class="label"
-                                               for="name">Character Name</label>
-                                        <div class="control">
-                                            <input v-model="formEntry.name"
-                                                   type="text"
-                                                   placeholder="enter a name"
-                                                   class="input"
-                                                   name="name"
-                                                   id="name"
-                                                   ref="name"
-                                                   autofocus />
-                                        </div>
-                                    </div>
-                                    <div class="field">
-                                        <label class="label"
-                                               for="score">Initiative
-                                            Score</label>
-                                        <div class="control">
-                                            <input v-model="formEntry.score"
-                                                   type="text"
-                                                   placeholder="enter a number"
-                                                   class="input"
-                                                   name="score"
-                                                   id="score"
-                                                   autocomplete="false" />
-                                        </div>
-                                    </div>
-                                    <div class="field is-grouped">
-                                        <div class="control is-expanded">
-                                            <label class="checkbox"
-                                                   for="npc">
-                                                <input type="checkbox"
-                                                       id="npc"
-                                                       name="npc"
-                                                       v-model="formEntry.npc">
-                                                NPC?</label>
-                                        </div>
-                                        <div class="control text-right">
-                                            <input value="add"
-                                                   type="submit"
-                                                   class="button is-primary">
-                                        </div>
-                                    </div>
-                                </form>
-                            </div>
+    <div id="app" class="container">
+        <div class="columns is-tablet">
+            <section class="column is-half-tablet is-one-third-desktop">
+                <div class="card">
+                    <div class="card-content">
+                        <h2 class="title">Add Character</h2>
+                        <div v-if="errors.length"
+                                class="message is-danger">
+                            <p class="message-body content">
+                                <strong>Please correct the following
+                                    error(s):</strong>
+                                <ul>
+                                    <li v-for="error in errors">
+                                        {{ error }}</li>
+                                </ul>
+                            </p>
                         </div>
-
-                    </section>
-                    <section
-                             class="column is-half-tablet is-two-thirds-desktop">
-                        <div class="card shadow-sm">
-                            <div class="card-content is-clearfix"
-                                 v-if="!loading">
-                                <h2 class="title">Initiative Order</h2>
-                                <em v-if="!currentRound.length && !nextRound.length"
-                                    class="text-muted">Add a character to get
-                                    started</em>
-
-                                <div v-if="currentRound.length">
-                                    <h3 class="subtitle is-marginless">Current
-                                        Round</h3>
-                                    <ul class="">
-                                        <li v-for="(char,index) in currentRoundSorted"
-                                            class="level is-marginless"
-                                            v-bind:class="{ 'has-text-danger': char.npc }">
-                                            <strong
-                                                    class="level-left">{{char.name}}</strong>
-                                            <div class="level-right">
-                                                <span
-                                                      class="level-item">{{char.score}}</span>
-                                                <span class="level-item"></span>
-                                                <button class="level-item"
-                                                        aria-label="delete"
-                                                        v-on:click="removeChar(char)">
-                                                    ⛔️</button>
-                                                <button class="button level-item  is-primary is-outlined "
-                                                        v-on:click="takeTurn(char)"
-                                                        v-bind:class="{'is-invisible': index !== 0}">👋
-                                                    Take
-                                                    Turn</button>
-                                            </div>
-                                        </li>
-                                    </ul>
+                        <form action=""
+                                @submit="addChar">
+                            <div class="field">
+                                <label class="label"
+                                        for="name">Character Name</label>
+                                <div class="control">
+                                    <input v-model="formEntry.name"
+                                            type="text"
+                                            placeholder="enter a name"
+                                            class="input"
+                                            name="name"
+                                            id="name"
+                                            ref="name"
+                                            autofocus />
                                 </div>
-                                <div v-if="nextRound.length">
-                                    <h3 class="subtitle is-marginless">Next
-                                        Round</h3>
-                                    <ul class="">
-                                        <li v-for="char in nextRoundSorted"
-                                            class="level is-marginless"
-                                            v-bind:class="{ 'has-text-danger': char.npc }">
-                                            <strong
-                                                    class="level-left">{{char.name}}</strong>
-                                            <div class="level-right">
-                                                <span
-                                                      class="level-item">{{char.score}}</span>
-                                                <span class="level-item"></span>
-                                                <button class="level-item"
-                                                        aria-label="delete"
-                                                        v-on:click="removeChar(char)">
-                                                    ⛔️</button>
-                                                <button
-                                                        class="button level-item  is-primary is-outlined is-invisible">👋
-                                                    Take
-                                                    Turn</button>
-                                            </div>
-                                        </li>
-                                    </ul>
+                            </div>
+                            <div class="field">
+                                <label class="label"
+                                        for="score">Initiative
+                                    Score</label>
+                                <div class="control">
+                                    <input v-model="formEntry.score"
+                                            type="text"
+                                            placeholder="enter a number"
+                                            class="input"
+                                            name="score"
+                                            id="score"
+                                            autocomplete="false" />
                                 </div>
-                                <button class="button  is-pulled-right"
-                                        v-if="currentRound.length && nextRound.length"
-                                        v-on:click="resetRound()">Reset
-                                    Round 💫</button>
                             </div>
-                            <div class="card-content has-text-centered"
-                                 v-if="loading">
-                                <progress class="progress is-large is-primary"
-                                          max="100"></progress>
-                                <strong class="has-text-primary">Restarting
-                                    Current Round</strong>
+                            <div class="field is-grouped">
+                                <div class="control is-expanded">
+                                    <label class="checkbox"
+                                            for="npc">
+                                        <input type="checkbox"
+                                                id="npc"
+                                                name="npc"
+                                                v-model="formEntry.npc">
+                                        NPC?</label>
+                                </div>
+                                <div class="control text-right">
+                                    <input value="add"
+                                            type="submit"
+                                            class="button is-primary">
+                                </div>
                             </div>
-                        </div>
-                    </section>
+                        </form>
+                    </div>
                 </div>
-            </div>
-        </main>
-        <footer class="footer text-right">Denise Nadal 2020</footer>
+
+            </section>
+            <section
+                        class="column is-half-tablet is-two-thirds-desktop">
+                <div class="card shadow-sm">
+                    <div class="card-content is-clearfix"
+                            v-if="!loading">
+                        <h2 class="title">Initiative Order</h2>
+                        <em v-if="!currentRound.length && !nextRound.length"
+                            class="text-muted">Add a character to get
+                            started</em>
+
+                        <div v-if="currentRound.length">
+                            <h3 class="subtitle is-marginless">Current
+                                Round</h3>
+                            <ul class="">
+                                <li v-for="(char,index) in currentRoundSorted"
+                                    class="level is-marginless"
+                                    v-bind:class="{ 'has-text-danger': char.npc }">
+                                    <strong
+                                            class="level-left">{{char.name}}</strong>
+                                    <div class="level-right">
+                                        <span
+                                                class="level-item">{{char.score}}</span>
+                                        <span class="level-item"></span>
+                                        <button class="level-item"
+                                                aria-label="delete"
+                                                v-on:click="removeChar(char)">
+                                            ⛔️</button>
+                                        <button class="button level-item  is-primary is-outlined "
+                                                v-on:click="takeTurn(char)"
+                                                v-bind:class="{'is-invisible': index !== 0}">👋
+                                            Take
+                                            Turn</button>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                        <div v-if="nextRound.length">
+                            <h3 class="subtitle is-marginless">Next
+                                Round</h3>
+                            <ul class="">
+                                <li v-for="char in nextRoundSorted"
+                                    class="level is-marginless"
+                                    v-bind:class="{ 'has-text-danger': char.npc }">
+                                    <strong
+                                            class="level-left">{{char.name}}</strong>
+                                    <div class="level-right">
+                                        <span
+                                                class="level-item">{{char.score}}</span>
+                                        <span class="level-item"></span>
+                                        <button class="level-item"
+                                                aria-label="delete"
+                                                v-on:click="removeChar(char)">
+                                            ⛔️</button>
+                                        <button
+                                                class="button level-item  is-primary is-outlined is-invisible">👋
+                                            Take
+                                            Turn</button>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                        <button class="button  is-pulled-right"
+                                v-if="currentRound.length && nextRound.length"
+                                v-on:click="resetRound()">Reset
+                            Round 💫</button>
+                    </div>
+                    <div class="card-content has-text-centered"
+                            v-if="loading">
+                        <progress class="progress is-large is-primary"
+                                    max="100"></progress>
+                        <strong class="has-text-primary">Restarting
+                            Current Round</strong>
+                    </div>
+                </div>
+            </section>
+        </div>
     </div>
 </template>
 
